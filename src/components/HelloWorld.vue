20231edb1bd17063313aea739d5a1648884b30cc
Shota Takahashi <108946581+ShohtaT@users.noreply.github.com>
2024-01-09 18:05:10 +0900

    初めてのAPI表示 (#2)
    * install axios
    
    * Show api successfully
    
    * pull_request_template.md
    
    ---------
    
    Co-authored-by: Takahashi Shota <shohh6119@gmial.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <p>{{ schedules }}</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +31,25 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      schedules: []
+    }
+  },
+  created() {
+    axios.defaults.withCredentials = true;
+    axios.defaults.baseURL = 'https://shihonet-api-stg-f80a0764e52a.herokuapp.com';
+    axios.get('/api/schedules')
+        .then(response => {
+          console.log(response.data);
+          this.schedules = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
   }
 }
 </script>
