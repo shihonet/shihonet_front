1fd5f7c9e977c23a6e8d7638d1f895d5390638a6
Takahashi Shota <shohh6119@gmial.com>
2024-01-09 18:01:59 +0900

    Add router-vue

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import TopPage from '../views/TopPage.vue'
+import BlogsPage from '../views/BlogsPage.vue'
+import SchedulesPage from '../views/SchedulesPage.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'main',
+        component: TopPage
+    },
+    {
+        path: '/blogs',
+        name: 'blogs',
+        component: BlogsPage
+    },
+    {
+        path: '/schedules',
+        name: 'schedules',
+        component: SchedulesPage
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
