d9ac49aefa5c9c105b133af43be695234da4bae2
Takahashi Shota <shohh6119@gmial.com>
2024-01-09 20:37:55 +0900

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
 </script>
 
 <style>
