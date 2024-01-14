f8029e52e14c6799b2f737397e3a9dc47350de31
Takahashi Shota <shohh6119@gmial.com>
2024-01-14 14:08:52 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,3 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
-
-// 認証の状態をローカルストレージから取得
-const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-
-// 認証ガードを追加
-router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth) && !isAuthenticated) {
-        // 認証が必要でログインしていない場合、ログインページにリダイレクト
-        next('/login');
-    } else {
-        next();
-    }
-})
