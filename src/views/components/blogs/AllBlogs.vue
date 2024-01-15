fbbe9d9edd1ae4ea0e971da1f75213c5e1f4ce69
Takahashi Shota <shohh6119@gmial.com>
2024-01-15 15:13:31 +0900

    [WIP] infinite scroll

--- src/views/components/blogs/AllBlogs.vue
+++ src/views/components/blogs/AllBlogs.vue
@@ -1,12 +1,27 @@
 <template>
-  <p>{{ blogs }}</p>
+  <div v-for="(blog, index) in blogs" :key="index" class="mt-6">
+    <FadeInOnScroll>
+      <div class="relative mx-6">
+        <img :src="blog.thumbnail_image_url" class="rounded-lg w-full">
+        <a :href="'https://www.hinatazaka46.com' + blog.url_path">
+          <div class="absolute bottom-0 left-0 right-0 bg-shiho-color bg-opacity-50 text-white px-4 py-2 rounded-lg">
+            <div class="text-[12px] font-extrabold">{{ blog.published_at }}</div>
+            <div class="text-[16px] font-extrabold">{{ blog.title }}</div>
+          </div>
+        </a>
+      </div>
+    </FadeInOnScroll>
+  </div>
+  <div ref="scrollTrigger" class="mt-10 text-center">読み込み中</div>
 </template>
 
 <script lang="ts">
 import axios from 'axios';
 import { defineComponent, PropType } from "vue";
+import FadeInOnScroll from "@/views/components/common/FadeInOnScroll.vue";
 
 export default defineComponent({
+  components: {FadeInOnScroll},
   props: {
     member: {
       type: String as PropType<string>
@@ -14,26 +29,44 @@ export default defineComponent({
   },
   data() {
     return {
-      blogs: [] as any[]
+      blogs: [] as any[],
+      page: 1,
+      limit: 4
+    }
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('/api/blogs', {
+          params: {
+            member: this.member,
+            page: this.page,
+            limit: this.limit,
+          }
+        });
+        this.blogs = this.blogs.concat(response.data.blogs);
+        this.page++; // Increment the page for the next request
+        console.log(`Fetched ${response.data.blogs.length} blogs. Total: ${this.blogs.length}`);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+    handleScroll() {
+      const scrollTrigger = this.$refs.scrollTrigger as HTMLDivElement;
+      const scrollPosition = window.scrollY + window.innerHeight;
+      const totalHeight = scrollTrigger.scrollHeight;
+
+      if (scrollPosition >= totalHeight - 300) {
+        // this.fetchData();
+      }
     }
   },
   created() {
     axios.defaults.withCredentials = true;
     axios.defaults.baseURL = 'https://shihonet-api-29ca225d2dcb.herokuapp.com/';
-    axios.get('/api/blogs', {
-      params: {
-        member: this.member,
-        page: 1,
-        limit: 15
-      }
-    })
-        .then(response => {
-          this.blogs = response.data;
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
-  }
+    window.addEventListener('scroll', this.handleScroll);
+    this.fetchData(); // Initial data fetch
+  },
 });
 </script>
 
