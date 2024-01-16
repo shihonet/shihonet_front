b3adde1cb9cfc806c9b1a5f3d835187398120841
Takahashi Shota <shohh6119@gmial.com>
2024-01-16 20:07:51 +0900

    Add withCredentials: true in file

--- src/views/components/tops/SchedulesPage.vue
+++ src/views/components/tops/SchedulesPage.vue
@@ -58,13 +58,17 @@ export default defineComponent({
     const formattedEndDate = endDate.toISOString().split("T")[0];
 
     axios
-      .get("/api/schedules", {
-        params: {
-          member_pattern_number: 3,
-          started_date: startDate,
-          ended_date: formattedEndDate,
-        },
-      })
+      .get(
+        "/api/schedules",
+        {
+          withCredentials: true,
+          params: {
+            member_pattern_number: 3,
+            started_date: startDate,
+            ended_date: formattedEndDate,
+          },
+        }
+      )
       .then((response: AxiosResponse) => {
         this.schedules = response.data.schedules;
       })
