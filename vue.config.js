8619cfd7b7403f4eb318aaa718e132d356cd3bdd
Takahashi Shota <shohh6119@gmial.com>
2024-01-16 21:38:15 +0900

    Fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: true,
   devServer: {
     proxy: {
@@ -8,4 +7,4 @@ module.exports = defineConfig({
       }
     }
   }
-})
+}
