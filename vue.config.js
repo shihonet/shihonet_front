ab75fc559dd01f1400db21f47f2ac14e0385a09c
Takahashi Shota <shohh6119@gmial.com>
2024-01-16 14:16:24 +0900

    set cors

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "https://toshikyon-fansite-9a0912894d4b.herokuapp.com/": {
+        target: "https://shihonet-api-29ca225d2dcb.herokuapp.com/",
+      }
+    }
+  }
 })
