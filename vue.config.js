c7e3bd7eccf735ffabb3c2db01920177a4b057b5
Takahashi Shota <shohh6119@gmial.com>
2024-01-16 21:34:24 +0900

    Fix

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
-      "https://toshikyon-fansite-9a0912894d4b.herokuapp.com/": {
-        target: "https://shihonet-api-29ca225d2dcb.herokuapp.com/",
+      "https://shihonet-api-29ca225d2dcb.herokuapp.com/": {
+        target: "https://toshikyon-fansite-9a0912894d4b.herokuapp.com/",
       }
     }
   }
