6a9dd636ffb9fc7e207e2de28ce1afb60d1665e1
Takahashi Shota <shohh6119@gmial.com>
2024-01-23 14:08:22 +0900

    Get histories

--- src/views/components/histories/AllHistories.vue
+++ src/views/components/histories/AllHistories.vue
@@ -1,15 +1,42 @@
 <template>
-  <FadeInOnScroll>
-    HISTORIES
-  </FadeInOnScroll>
+  <div class="mx-6">
+    <FadeInOnScroll>
+      <div v-for="(history, index) in histories" :key="index" class="mt-6">
+        <p>{{ history.name }}</p>
+        <p>{{ history.started_date }}</p>
+        <p>{{ history.category_name }}</p>
+        <p></p>
+      </div>
+    </FadeInOnScroll>
+  </div>
 </template>
 
 <script lang="ts">
-import FadeInOnScroll from "@/views/components/common/FadeInOnScroll.vue";
+import axios from "axios";
 import { defineComponent } from "vue";
+import FadeInOnScroll from "@/views/components/common/FadeInOnScroll.vue";
 
 export default defineComponent({
   components: { FadeInOnScroll },
+  data() {
+    return {
+      histories: [] as any[],
+    };
+  },
+  created() {
+    axios
+      .get("/api/histories", {
+        params: {
+          year: 2019,
+        },
+      })
+      .then((response) => {
+        this.histories = response.data.histories;
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  },
 });
 </script>
 
