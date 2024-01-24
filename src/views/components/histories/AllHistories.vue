b85a424bc010156d6229f4ac1ecafd7950c0ca9c
Shota Takahashi <108946581+ShohtaT@users.noreply.github.com>
2024-01-24 18:39:48 +0900

    年ごとにhistoriesを表示 (#30)
    * Impl select box
    
    * Fix
    
    ---------
    
    Co-authored-by: Takahashi Shota <shohh6119@gmial.com>

--- src/views/components/histories/AllHistories.vue
+++ src/views/components/histories/AllHistories.vue
@@ -1,37 +1,43 @@
 <template>
   <div class="mx-6">
-    <div
-      v-for="(histories_of_year, year) in histories"
-      :key="year"
-      class="mb-10"
-    >
-      <FadeInOnScroll>
-        <h1 class="text-[20px] font-extrabold text-site-color">◆{{ year }}年</h1>
-      </FadeInOnScroll>
+    <FadeInOnScroll>
+      <h1 class="text-[20px] font-extrabold text-site-color">
+        ◆<span class="mx-2 font-bold">Activities of</span>
+        <select
+          v-model="selectedYear"
+          @change="handleYearChange"
+          id="year"
+          class="w-24"
+        >
+          <option v-for="year in availableYears" :key="year" :value="year">
+            {{ year }}
+          </option>
+        </select>
+      </h1>
+    </FadeInOnScroll>
 
-      <div class="flex">
-        <div class="w-5">
-          <div
-            class="w-1 mx-auto h-full rounded-lg bg-gradient-to-b from-green-200 to-blue-200"
-          ></div>
-        </div>
+    <div v-if="loading">
+      <WaitingForLoading />
+    </div>
 
-        <div class="w-full ml-2">
-          <div
-            v-for="(history, index) in histories_of_year"
-            :key="index"
-            class="mb-2"
-          >
-            <FadeInOnScroll>
-              <a :href="'https://www.hinatazaka46.com' + history.url_path">
-                <p class="font-light text-site-color">
-                  {{ history.started_date }}【{{ history.category_name }}】
-                </p>
-                <p class="font-normal">{{ history.name }}</p>
-              </a>
-              <span class="block w-full border-b border-gray-200 my-1"></span>
-            </FadeInOnScroll>
-          </div>
+    <div v-else class="flex mt-2">
+      <div class="w-5">
+        <div
+          class="w-1 mx-auto h-full rounded-lg bg-gradient-to-b from-blue-200 to-green-200"
+        ></div>
+      </div>
+
+      <div class="w-full ml-2">
+        <div v-for="(history, index) in histories" :key="index" class="mb-2">
+          <FadeInOnScroll>
+            <a :href="'https://www.hinatazaka46.com' + history.url_path">
+              <p class="font-light text-site-color">
+                {{ history.started_date }}【{{ history.category_name }}】
+              </p>
+              <p class="font-normal">{{ history.name }}</p>
+            </a>
+            <span class="block w-full border-b border-gray-200 my-1"></span>
+          </FadeInOnScroll>
         </div>
       </div>
     </div>
@@ -42,23 +48,38 @@
 import axios from "axios";
 import { defineComponent } from "vue";
 import FadeInOnScroll from "@/views/components/common/FadeInOnScroll.vue";
+import WaitingForLoading from "@/views/components/common/WaitingForLoading.vue";
 
 export default defineComponent({
-  components: { FadeInOnScroll },
+  components: { WaitingForLoading, FadeInOnScroll },
   data() {
     return {
+      loading: false,
       histories: [] as any[],
+      availableYears: [2024, 2023, 2022, 2021, 2020, 2019],
+      selectedYear: 2024,
     };
   },
   created() {
-    axios
-      .get("/api/histories")
-      .then((response) => {
-        this.histories = response.data.histories;
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    this.handleYearChange();
+  },
+  methods: {
+    handleYearChange() {
+      this.loading = true;
+      axios
+        .get("/api/histories", {
+          params: {
+            year: this.selectedYear,
+          },
+        })
+        .then((response) => {
+          this.histories = response.data.histories;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    },
   },
 });
 </script>
