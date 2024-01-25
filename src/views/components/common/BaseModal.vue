8494cd75e0d1cc847dbe884aea056448203a4a5c
Shota Takahashi <108946581+ShohtaT@users.noreply.github.com>
2024-01-25 20:22:01 +0900

    生誕用モーダルを表示 (#32)
    * Impl show modal
    
    * Impl close modal
    
    * Pass elements from outside
    
    ---------
    
    Co-authored-by: Takahashi Shota <shohh6119@gmial.com>

---
+++ src/views/components/common/BaseModal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-show="isModalOpen">
+    <div id="base_modal" class="fixed z-50 inset-0 overflow-y-auto">
+      <FadeInOnScroll>
+        <div class="flex items-center justify-center min-h-screen">
+          <div class="flex flex-col w-80">
+            <div class="w-full mb-2 text-white text-[32px] text-right">
+              <span @click="closeModal" class="hover:cursor-pointer">×</span>
+            </div>
+            <div class="w-full h-[540px]">
+              <slot></slot>
+            </div>
+          </div>
+        </div>
+      </FadeInOnScroll>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FadeInOnScroll from "@/views/components/common/FadeInOnScroll.vue";
+
+export default defineComponent({
+  components: { FadeInOnScroll },
+  data() {
+    return {
+      isModalOpen: true,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.isModalOpen = false;
+    }
+  }
+});
+</script>
+
+<style scoped>
+/* Google fontsで導入したいfonts から@importのコードを取得 */
+/* → https://fonts.google.com/specimen/M+PLUS+Rounded+1c?query=rounded */
+@import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&display=swap");
+
+#base_modal {
+  /* Google fontsで導入したいfontsからfont-familyを取ってくる */
+  font-family: "M PLUS Rounded 1c", sans-serif;
+  max-width: 480px;
+  margin: 0 auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
--- src/views/pages/TopPage.vue
+++ src/views/pages/TopPage.vue
@@ -1,6 +1,11 @@
 <template>
+  <!-- 生誕モーダル-->
+  <BaseModal>
+    <img src="@/assets/images/happy_birthday_modal.png" class="h-full w-full rounded-[40px] shadow-2xl"/>
+  </BaseModal>
+
   <FadeInOnScroll>
-    <img src="../../assets/images/top_image.png" />
+    <img src="@/assets/images/top_image.png" />
   </FadeInOnScroll>
   <DetailProfiles />
   <Schedules />
@@ -13,9 +18,11 @@ import Schedules from "@/views/components/top/TopSchedules.vue";
 import Blogs from "@/views/components/top/TopBlogs.vue";
 import DetailProfiles from "@/views/components/top/ToplProfiles.vue";
 import { defineComponent } from "vue";
+import BaseModal from "@/views/components/common/BaseModal.vue";
 
 export default defineComponent({
   components: {
+    BaseModal,
     DetailProfiles,
     Blogs,
     Schedules,
