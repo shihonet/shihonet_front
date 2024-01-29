cd351a73fd75cbf879c9bc80b576623056217b09
Takahashi Shota <shohh6119@gmial.com>
2024-01-30 00:40:17 +0900

    Impl cracker animation

--- src/views/pages/TopPage.vue
+++ src/views/pages/TopPage.vue
@@ -19,6 +19,7 @@ import Blogs from "@/views/components/top/TopBlogs.vue";
 import DetailProfiles from "@/views/components/top/ToplProfiles.vue";
 import { defineComponent } from "vue";
 import BaseModal from "@/views/components/common/BaseModal.vue";
+import confetti from 'canvas-confetti';
 
 export default defineComponent({
   components: {
@@ -27,6 +28,19 @@ export default defineComponent({
     Blogs,
     Schedules,
     FadeInOnScroll,
+  },
+  mounted() {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.3.2/dist/confetti.browser.min.js';
+    script.onload = () => {
+      // @ts-ignore
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+      });
+    };
+    document.head.appendChild(script);
   },
 });
 </script>
