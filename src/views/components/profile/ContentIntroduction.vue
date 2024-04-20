03abb660d8345bfe901e267a2576aa6a759b1ef5
Takahashi Shota <shohh6119@gmial.com>
2024-04-20 17:22:44 +0900

    Impl histories of profile

--- src/views/components/profile/ContentIntroduction.vue
+++ src/views/components/profile/ContentIntroduction.vue
@@ -1,5 +1,184 @@
 <template>
-  <img src="@/assets/images/histories_title.svg" class="w-40">
+  <FadeInOnScroll>
+    <img src="@/assets/images/histories_title.svg" class="w-40" />
+  </FadeInOnScroll>
 
+  <div class="mt-6">
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">1998年2月2日</p>
+        <div class="mt-2 mx-4">
+          <p>3人姉妹の次女として誕生。（東京都出身）</p>
+        </div>
+      </div>
+    </FadeInOnScroll>
 
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2016年5月8日</p>
+        <div class="mt-2 mx-4">
+          <p>けやき坂46 メンバーオーディションに合格。</p>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2018年8月15日</p>
+        <div class="mt-2 mx-4">
+          <p class="text-[20px] text-site-color">
+            初センター『ハッピーオーラ』発売
+          </p>
+          <iframe
+            src="https://www.youtube.com/embed/eQBtQTPCyV4?si=LbDCS-dOjr6Vur4A"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            class="mt-4 w-full h-60"
+          ></iframe>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2019年2月11日</p>
+        <div class="mt-2 mx-4">
+          <p>けやき坂46から日向坂46に改称。</p>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2019年3月27日</p>
+        <div class="mt-2 mx-4">
+          <p>
+            デビューシングル「キュン」発売をもって、日向坂46として単独デビュー。
+          </p>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2021年5月26日</p>
+        <div class="mt-2 mx-4">
+          <p class="text-[20px] text-site-color">
+            5thSG『君しか勝たん』でセンターに
+          </p>
+          <iframe
+            src="https://www.youtube.com/embed/Z59HsgPVbWY?si=pw2mKbz8lBc3EoB0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            class="mt-4 w-full h-60"
+          ></iframe>
+          <p class="mt-2">
+            日向坂46に改名してから初となるセンター楽曲。<br />
+            5thシングルに収録されている個人PVにて、加藤史帆の個人PV
+            <a
+              href="https://youtu.be/d4V0nAhncMg?si=s09C4C_u-kE6GIAv"
+              target="_blank"
+              class="font-medium text-blue-500 underline"
+              >『5年間』</a
+            >も収録されている。
+          </p>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2023年6月17日</p>
+        <div class="mt-2 mx-4">
+          <p class="text-[20px] text-site-color">
+            かとし×こさかなユニバデート【Vlog】
+          </p>
+          <iframe
+            src="https://www.youtube.com/embed/-gRzsVn5AzM?si=gjg9ScYbu0A97LRL"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            class="mt-4 w-full h-60"
+          ></iframe>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2023年6月20日</p>
+        <div class="mt-2 mx-4">
+          <p>
+            加藤史帆1st写真集「#会いたい」発売。<br />
+            撮影場所は本人が昔から切望していたハワイ・オアフ島。
+          </p>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2023年6月17日</p>
+        <div class="mt-2 mx-4">
+          <p class="text-[20px] text-site-color">
+            不幸が続くのでお祓い行ってきた【ハッピー開運ツアー】
+          </p>
+          <iframe
+            src="https://www.youtube.com/embed/sSWjfvUNDF4?si=v9di53trMeHwz1iy"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            class="mt-4 w-full h-60"
+          ></iframe>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2024年2月11日</p>
+        <div class="mt-2 mx-4">
+          <p class="text-[20px] text-site-color">
+            日向坂のリトルトゥースがリアル脱出ゲームに挑戦してみた
+          </p>
+          <iframe
+            src="https://www.youtube.com/embed/vlWcVTOFyMw?si=ORPrJx6K7I3Ant7h"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            class="mt-4 w-full h-60"
+          ></iframe>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2024年3月8日</p>
+        <div class="mt-2 mx-4">
+          <p class="mt-2">
+            加藤史帆 初主演ドラマ
+            <a
+              href="https://www.tv-tokyo.co.jp/haihaji/"
+              target="_blank"
+              class="font-medium text-blue-500 underline"
+            >「これから配信始めます」</a
+            > 放送開始。（テレビ東京）<br />
+            全四話のオムニバスドラマで、かすみ役として、第二話では主演を務めた。
+          </p>
+        </div>
+      </div>
+    </FadeInOnScroll>
+  </div>
 </template>
+
+<script setup lang="ts">
+import FadeInOnScroll from "@/views/components/common/FadeInOnScroll.vue";
+</script>
