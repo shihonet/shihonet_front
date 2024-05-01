4beabc066fe3913e8d440f70a10efe9d9c5f61f0
Shota Takahashi <108946581+ShohtaT@users.noreply.github.com>
2024-05-01 18:36:12 +0900

    他ファンサイトの動線をおく/メニューCSS修正 (#51)
    * Insert kyonkosite, nibupage in footer
    
    * Arrange FadeInOnScroll in Profiles
    
    * Fix text
    
    * Fix css
    
    ---------
    
    Co-authored-by: Takahashi Shota <shohh6119@gmial.com>

--- src/views/components/common/PageFooter.vue
+++ src/views/components/common/PageFooter.vue
@@ -3,27 +3,38 @@
     <div class="w-full bg-site-color px-8 pt-10 pb-10 text-white">
       <ul class="text-[20px] font-normal">
         <li class="pb-4">
-          <RouterLink to="/profiles">Profiles</RouterLink>
+          <RouterLink to="/profiles" class="hover:opacity-80">Profiles</RouterLink>
         </li>
         <li class="pb-4">
-          <RouterLink to="/blogs">Blogs</RouterLink>
+          <RouterLink to="/blogs" class="hover:opacity-80">Blogs</RouterLink>
         </li>
         <li class="pb-4">
-          <RouterLink to="/histories">Histories</RouterLink>
+          <RouterLink to="/histories" class="hover:opacity-80">Histories</RouterLink>
         </li>
         <li class="pb-4">
-          <RouterLink to="/about">About "#shihonet"</RouterLink>
+          <RouterLink to="/about" class="hover:opacity-80">About "#shihonet"</RouterLink>
         </li>
         <li class="pb-4">
-          <RouterLink to="/thanks_post">Thanks Generator</RouterLink>
+          <RouterLink to="/thanks_post" class="hover:opacity-80">Thanks Generator</RouterLink>
         </li>
         <li class="pb-4">
-          <a href="https://www.hinatazaka46.com/s/official/?ima=0000"
-          >日向坂46 Official Web Site</a
-          >
+          <a href="https://www.hinatazaka46.com/s/official/?ima=0000" class="hover:opacity-80" target="_blank">
+            日向坂46 Official Web Site
+          </a>
         </li>
       </ul>
-      <div class="mt-14 text-[8px] text-center">
+      <div class="mt-10 text-[16px]">
+        <p>〜他ファンサイト〜</p>
+        <ul>
+          <li class="mt-1">
+            - <a href="https://nibuparu2286.wixsite.com/mysite" target="_blank" class="hover:opacity-80">#nibupage（丹生明里さん/日向坂46）</a>
+          </li>
+          <li class="mt-1">
+            - <a href="https://ruinu6446.wixsite.com/my-site-3" target="_blank" class="hover:opacity-80">#kyonkosite（齊藤京子さん/日向坂46OG）</a>
+          </li>
+        </ul>
+      </div>
+      <div class="mt-16 text-[8px] text-center">
         <p>© 2024 #shihonet, developing by @shihonet.</p>
         <p>
           このサイトは、Seed & Flower合同会社その他各種権利者から許諾等を得ているものではありません。
