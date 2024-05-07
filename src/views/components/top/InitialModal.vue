ba99c5b7939601419a711066ab0ac96ded2a5db6
Takahashi Shota <shohh6119@gmial.com>
2024-05-08 02:29:47 +0900

    Impl modal for けやき坂46結成8周年

--- src/views/components/top/InitialModal.vue
+++ src/views/components/top/InitialModal.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    モーダル表示内容
+  <div class="relative">
+    <img
+      src="@/assets/images/keyakizaka_8th_aniversary.png"
+      class="w-full h-auto"
+    />
+    <div class="absolute inset-0 flex items-end justify-center">
+      <div class="mb-16 text-sky-400 font-bold text-center">
+        <p class="text-xl">~Happy Anniversary~</p>
+        <p class="text-3xl">#けやき坂46結成8周年</p>
+      </div>
+    </div>
   </div>
+  <a
+    href="https://twitter.com/intent/tweet?text=%E3%81%91%E3%82%84%E3%81%8D%E5%9D%8246%E3%80%81%E7%B5%90%E6%88%908%E5%91%A8%E5%B9%B4%E3%81%8A%E3%82%81%E3%81%A7%E3%81%A8%E3%81%86%EF%BC%81%0A%23%E3%81%91%E3%82%84%E3%81%8D%E5%9D%8246%E7%B5%90%E6%88%908%E5%91%A8%E5%B9%B4%20%F0%9F%8E%89%0A%23%E6%97%A5%E5%90%91%E5%9D%8246%20%23%E5%8A%A0%E8%97%A4%E5%8F%B2%E5%B8%86%20%E3%81%A1%E3%82%83%E3%82%93%20%F0%9F%90%BB%0A%23shihonet%20"
+    target="_blank"
+    class="mt-4 flex justify-center"
+  >
+    <div
+      class="flex items-center justify-center bg-black font-bold py-2 px-4 rounded-full w-[108px]"
+    >
+      <div class="flex items-center">
+        <img src="@/assets/images/x_logo.svg" class="h-4 mr-2" />
+        <span class="font-normal text-white">ポスト</span>
+      </div>
+    </div>
+  </a>
 </template>
