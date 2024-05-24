ad47a44d821248cc83289ed134860313fa165eaa
Takahashi Shota <shohh6119@gmial.com>
2024-05-25 00:57:33 +0900

    Add blogUrl to thanks_generator_post_text

--- src/views/components/thanksPost/AllThanksPost.vue
+++ src/views/components/thanksPost/AllThanksPost.vue
@@ -35,7 +35,7 @@
         <p class="text-center mt-10 text-[12px]">上の画像をコピーまたは保存して、ポストしよう！</p>
         <div class="flex justify-center mt-4">
           <PostX
-            link="https://x.com/intent/post?text=%E4%BB%8A%E6%97%A5%E3%82%82%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E3%81%A7%E3%81%84%E3%81%A6%E3%81%8F%E3%82%8C%E3%81%A6%E3%81%82%E3%82%8A%E3%81%8C%E3%81%A8%E3%81%86%F0%9F%AB%B6%0A%23%E6%97%A5%E5%90%91%E5%9D%8246%20%23%E5%8A%A0%E8%97%A4%E5%8F%B2%E5%B8%86%20%E3%81%A1%E3%82%83%E3%82%93%20%F0%9F%90%BB%0A%23shihonet"
+            link="https://x.com/intent/post?text=%E4%BB%8A%E6%97%A5%E3%82%82%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E3%81%A7%E3%81%84%E3%81%A6%E3%81%8F%E3%82%8C%E3%81%A6%E3%81%82%E3%82%8A%E3%81%8C%E3%81%A8%E3%81%86%F0%9F%AB%B6%0A%23%E6%97%A5%E5%90%91%E5%9D%8246%20%23%E5%8A%A0%E8%97%A4%E5%8F%B2%E5%B8%86%20%E3%81%A1%E3%82%83%E3%82%93%20%F0%9F%90%BB%0A%23shihonet%0A#{{ blog.blogUrl }}"
           />
         </div>
       </div>
