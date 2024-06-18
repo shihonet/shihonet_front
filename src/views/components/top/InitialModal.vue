3533f9ef9b83688bb5162c5a59f24ada7c1d77ff
Takahashi Shota <shohh6119@gmial.com>
2024-06-18 21:38:28 +0900

    Impl 20240620_aitai_modal

--- src/views/components/top/InitialModal.vue
+++ src/views/components/top/InitialModal.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <img src="@/assets/images/kimishikakatan_3rd_modal_img.svg" class="max-w-rounded-3xl shadow-2xl" />
+    <img src="@/assets/images/20240620_aitai_modal_img.png" class="max-w-rounded-3xl shadow-2xl" />
   </div>
 </template>
