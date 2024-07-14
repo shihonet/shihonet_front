b3d437cd6e5448b8c93704ef1326cb66e7ef57dc
Takahashi Shota <shohh6119@gmial.com>
2024-07-14 16:59:17 +0900

    Fix ui

--- src/views/components/blogs/show/AllShowBlogs.vue
+++ src/views/components/blogs/show/AllShowBlogs.vue
@@ -5,8 +5,24 @@
     </div>
     <div v-else>
       <p class="text-2xl font-bold">{{ blog.title }}</p>
-      <p class="mt-2 text-gray-400">{{ blog.publishedAt }}</p>
-      <p class="mt-2.5 whitespace-break-spaces">{{ blog.content }}</p>
+      <p class="mt-2 text-[12px] text-gray-400 flex justify-end">{{ blog.publishedAt }}</p>
+      <p class="mt-6 whitespace-break-spaces" v-html="formattedContent"></p>
+      <hr class="my-16">
+      <p class="text-[12px] text-gray-400 flex justify-center">画像一覧（{{ blog.imageUrls?.length }}枚）</p>
+      <div v-for="(image, index) in blog.imageUrls" :key="index" class="mt-2 mb-5 mx-4">
+        <img :src="image" class="w-full" />
+      </div>
+      <div class="mt-16 flex justify-end">
+        <a :href="blog.blogUrl" class="text-blue-500 underline" target="_blank">公式ブログ ＞</a>
+      </div>
+    </div>
+    <div class="mt-20 flex items-center justify-center">
+      <RouterLink
+        to="/blogs"
+        class="bg-white font-bold py-3 px-6 rounded-full border border-gray-500 hover:opacity-80"
+      >
+        一覧へ戻る
+      </RouterLink>
     </div>
   </FadeInOnScroll>
 </template>
@@ -33,9 +49,17 @@ export default defineComponent({
       blogShowStore.requestFetchBlog(props.id);
     });
 
+    // テキスト中のURLをリンクに変換する
+    const formatContent = (content: string): string => {
+      if (!content) return '';
+      const urlRegex = /((https?:\/\/[^\s]+))/g;
+      return content.replace(urlRegex, '<a href="$1" target="_blank" class="text-blue-500 underline">$1</a>');
+    };
+
     return {
       blog: computed(() => blogShowStore.getBlog),
       isLoading: computed(() => blogShowStore.getIsLoading),
+      formattedContent: computed(() => formatContent(blogShowStore.getBlog.content)),
     };
   },
 });
--- src/views/components/thanksPost/AllThanksPost.vue
+++ src/views/components/thanksPost/AllThanksPost.vue
@@ -14,7 +14,7 @@
     <div v-else class="flex items-center justify-center">
       <button
         v-if="!isClickedButton"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-10"
+        class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full mt-10"
         @click="requestGetRandomBlogs"
       >
         画像ガチャを回す
