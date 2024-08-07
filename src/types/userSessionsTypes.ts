401407c51da5fb42e632d989d669b1fd4643b227
Takahashi Shota <shohh6119@gmial.com>
2024-08-07 20:42:01 +0900

    Impl requestGetUserSessions

---
+++ src/types/userSessionsTypes.ts
@@ -0,0 +1,15 @@
+export interface User {
+  isLoggedIn: boolean,
+  id: number | undefined,
+  email: string | undefined,
+  displayName: string | undefined,
+  lastLoggedInAt: string | undefined,
+}
+
+export interface ApiResponseUser {
+  is_logged_in: boolean,
+  id: number | undefined,
+  email: string | undefined,
+  display_name: string | undefined,
+  last_logged_in_at: string | undefined,
+}
