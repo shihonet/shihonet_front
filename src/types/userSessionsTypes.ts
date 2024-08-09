87f2c9ebeee7f29f9fcf613aa55e4cd868d592b8
Shota Takahashi <108946581+ShohtaT@users.noreply.github.com>
2024-08-09 23:32:08 +0900

    csrfToken をヘッダに含める (#75)
    * Impl
    
    * Run prittier
    
    ---------
    
    Co-authored-by: Takahashi Shota <shohh6119@gmial.com>

--- src/types/userSessionsTypes.ts
+++ src/types/userSessionsTypes.ts
@@ -1,15 +1,17 @@
 export interface User {
-  isLoggedIn: boolean,
-  id: number | undefined,
-  email: string | undefined,
-  displayName: string | undefined,
-  lastLoggedInAt: string | undefined,
+  csrfToken: string;
+  isLoggedIn: boolean;
+  id: number | undefined;
+  email: string | undefined;
+  displayName: string | undefined;
+  lastLoggedInAt: string | undefined;
 }
 
 export interface ApiResponseUser {
-  is_logged_in: boolean,
-  id: number | undefined,
-  email: string | undefined,
-  display_name: string | undefined,
-  last_logged_in_at: string | undefined,
+  csrf_token: string;
+  is_logged_in: boolean;
+  id: number | undefined;
+  email: string | undefined;
+  display_name: string | undefined;
+  last_logged_in_at: string | undefined;
 }
