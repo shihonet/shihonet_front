d401f67a6ae4a8b85d10faa8d14c543fc50b1a38
Takahashi Shota <shohh6119@gmial.com>
2024-08-09 23:25:13 +0900

    Impl

--- src/main.ts
+++ src/main.ts
@@ -6,46 +6,37 @@ import { createPinia } from "pinia";
 import { createPersistedState } from "pinia-plugin-persistedstate";
 import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
-import ToastService from 'primevue/toastservice';
-import Toast from 'primevue/toast';
+import ToastService from "primevue/toastservice";
+import Toast from "primevue/toast";
 import "primeicons/primeicons.css";
 import { useUserSessionsStore } from "@/stores/userSessionsStore";
-
-axios.defaults.withCredentials = true;
-axios.defaults.xsrfHeaderName = "X-CSRF-Token";
-axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
-
-// CSRFトークンをメタタグから取得してAxiosに設定
-const metaTag = document.querySelector('meta[name="csrf-token"]');
-if (metaTag) {
-  const csrfToken = metaTag.getAttribute("content");
-  if (csrfToken) {
-    axios.defaults.headers.common["X-CSRF-Token"] = csrfToken;
-  } else {
-    console.error(
-      "CSRF token meta tag found, but content attribute is missing."
-    );
-  }
-} else {
-  console.error("CSRF token meta tag is missing.");
-}
 
 // INFO: store 永続化の persist の追加
 const pinia = createPinia();
 pinia.use(createPersistedState());
 
 createApp(App)
-  .use(router)
-  .use(pinia)
-  .use(PrimeVue, {
-    theme: {
-      preset: Aura,
-    },
-  })
-  .use(ToastService)
-  .component('Toast', Toast)
-  .mount("#app");
+    .use(router)
+    .use(pinia)
+    .use(PrimeVue, {
+      theme: {
+        preset: Aura,
+      },
+    })
+    .use(ToastService)
+    .component("Toast", Toast)
+    .mount("#app");
+
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfHeaderName = "X-CSRF-Token";
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 
 // ログインユーザーの情報を取得
 const userSessionsStore = useUserSessionsStore();
-userSessionsStore.requestGetUserSessions();
+await userSessionsStore.requestGetUserSessions();
+
+const csrfToken = userSessionsStore.getCsrfToken;
+axios.defaults.headers.common = {
+  "X-Requested-With": "XMLHttpRequest",
+  "X-CSRF-TOKEN": csrfToken,
+};
