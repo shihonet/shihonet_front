e4f272b0005c68609f81770a98abb1fd25806256
Takahashi Shota <shohh6119@gmial.com>
2024-08-12 01:01:30 +0900

    Filter loading in update favorite

--- src/stores/favoriteBlogsStore.ts
+++ src/stores/favoriteBlogsStore.ts
@@ -5,9 +5,16 @@ import { useBlogShowStore } from "@/stores/blogShowStore";
 import { useOpenStore } from "@/stores/openStore";
 
 export const useFavoriteBlogsStore = defineStore("favoriteBlogs", {
+  state: () => ({
+    isLoading: false,
+  }),
+
   actions: {
     // ブログ一覧からお気に入りを更新する場合のリクエスト
     async requestPutFavoriteBlogInList(blogId: number) {
+      if (this.isLoading) return;
+
+      this.isLoading = true;
       const blogsStore = useBlogsStore();
       const openStore = useOpenStore();
       try {
@@ -16,11 +23,16 @@ export const useFavoriteBlogsStore = defineStore("favoriteBlogs", {
       } catch (error) {
         openStore.setToast("error", "お気に入り登録に失敗しました。");
         console.error("Error fetching data:", error);
+      } finally {
+        this.isLoading = false;
       }
     },
 
     // ブログ詳細からお気に入りを更新する場合のリクエスト
     async requestPutFavoriteBlogInShow(blogId: number) {
+      if (this.isLoading) return;
+
+      this.isLoading = true;
       const blogShowStore = useBlogShowStore();
       const openStore = useOpenStore();
       try {
@@ -29,6 +41,8 @@ export const useFavoriteBlogsStore = defineStore("favoriteBlogs", {
       } catch (error) {
         openStore.setToast("error", "お気に入り登録に失敗しました。");
         console.error("Error fetching data:", error);
+      } finally {
+        this.isLoading = false;
       }
     },
   },
