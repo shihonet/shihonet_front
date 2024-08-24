26587c985e3456bfb4dd0de302e979aff88c9787
Takahashi Shota <shohh6119@gmial.com>
2024-08-24 16:23:48 +0900

    Fix profiles

--- src/views/components/profiles/AllProfiles.vue
+++ src/views/components/profiles/AllProfiles.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="mt-10 mb-10 mx-8">
-    <TitlePart title="Profiles" />
+    <TitlePart title="Shiho Kato" />
   </div>
   <p class="text-center text-[12px] font-light">Shiho Kato</p>
   <p class="mt-1 text-center text-[20px]">加藤 史帆</p>
   <p class="mt-4 text-center text-light text-site-color text-sm">
     日向坂46 一期生
   </p>
-  <img src="@/assets/images/12th_artist_img.png" class="mt-4 mx-auto w-[220px]" />
+  <img
+    src="https://cdn.hinatazaka46.com/images/14/610/d713c2e65eee8fa173ef6c62d0d49/800_800_102400.jpg"
+    class="mt-4 mx-auto w-[220px]"
+  />
   <ProfileTable :isNeededDetailProf="true" />
 
   <FadeInOnScroll>
-    <div class="mt-16 bg-yellow-100 rounded-lg py-4 px-2">
+    <div class="mt-16 border-2 border-green-200 rounded-lg py-4 px-2">
       <p class="text-[14px] font-light text-center">
         リアルタイムで加藤史帆ちゃんの近況を知りたい方は、
         <a
@@ -31,6 +34,10 @@
 </template>
 
 <script setup lang="ts">
-import { FadeInOnScroll, TitlePart, ProfileTable } from "@/views/components/common";
+import {
+  FadeInOnScroll,
+  TitlePart,
+  ProfileTable,
+} from "@/views/components/common";
 import { ContentIntroduction } from "@/views/components/profiles";
 </script>
--- src/views/components/profiles/ContentIntroduction.vue
+++ src/views/components/profiles/ContentIntroduction.vue
@@ -7,6 +7,7 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">1998年2月2日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p>3人姉妹の次女として誕生。（東京都出身）</p>
         </div>
@@ -16,6 +17,7 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2016年5月8日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p>けやき坂46 メンバーオーディションに合格。</p>
         </div>
@@ -25,6 +27,7 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2018年8月15日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p class="text-[20px] text-site-color font-bold">
             初センター『ハッピーオーラ』発売
@@ -35,7 +38,7 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
-            class="mt-4 w-full h-60"
+            class="mt-4 w-full h-52"
           ></iframe>
         </div>
       </div>
@@ -44,6 +47,7 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2019年2月11日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p>けやき坂46から日向坂46に改称。</p>
         </div>
@@ -53,6 +57,7 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2019年3月27日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p>
             デビューシングル「キュン」発売をもって、日向坂46として単独デビュー。
@@ -64,6 +69,7 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2021年5月26日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p class="text-[20px] text-site-color font-bold">
             5thSG『君しか勝たん』でセンターに
@@ -74,7 +80,7 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
-            class="mt-4 w-full h-60"
+            class="mt-4 w-full h-52"
           ></iframe>
           <p class="mt-2">
             日向坂46に改名してから初となるセンター楽曲。<br />
@@ -82,7 +88,7 @@
             <a
               href="https://youtu.be/d4V0nAhncMg?si=s09C4C_u-kE6GIAv"
               target="_blank"
-              class="font-medium text-blue-500 underline"
+              class="text-site-color hover:underline"
               >『5年間』</a
             >も収録されている。
           </p>
@@ -93,9 +99,10 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2023年6月17日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p>
-            【日向坂ちゃんねる】<br/>
+            【日向坂ちゃんねる】<br />
             『【お忍び】かとし×こさかなユニバデート【Vlog】』
           </p>
           <iframe
@@ -104,7 +111,7 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
-            class="mt-4 w-full h-60"
+            class="mt-4 w-full h-52"
           ></iframe>
         </div>
       </div>
@@ -113,39 +120,74 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2023年6月20日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p class="text-[20px] text-site-color font-bold">
             加藤史帆1st写真集「#会いたい」発売。
           </p>
           <p class="mt-2">
             撮影場所は本人が昔から切望していたハワイ・オアフ島！<br />
-            写真集公式Xアカウントは <a href="https://x.com/katoshi_aloha" target="_blank" class="text-blue-500 underline">こちら</a> 。
+            写真集公式Xアカウントは
+            <a
+              href="https://x.com/katoshi_aloha"
+              target="_blank"
+              class="text-site-color hover:underline"
+              >こちら</a
+            >
+            。
           </p>
           <div class="mt-2 mx-10">
-            <img src="https://cdn.hinatazaka46.com/files/14/tsujo.jpg" class="mt-4">
+            <img
+              src="https://cdn.hinatazaka46.com/files/14/tsujo.jpg"
+              class="mt-4"
+            />
           </div>
           <div class="mt-4 flex flex-col">
             <p class="text-center">ご購入はこちらから！</p>
             <div class="mt-4 flex space-x-4 justify-center">
-              <a href="https://amzn.asia/d/8VKrKAb" target="_blank" class="text-blue-500">
-                <div class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100">
+              <a
+                href="https://amzn.asia/d/8VKrKAb"
+                target="_blank"
+                class="text-blue-500"
+              >
+                <div
+                  class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100"
+                >
                   アマゾン
                 </div>
               </a>
-              <a href="https://books.rakuten.co.jp/rb/17495252/" target="_blank" class="text-blue-500">
-                <div class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100">
+              <a
+                href="https://books.rakuten.co.jp/rb/17495252/"
+                target="_blank"
+                class="text-blue-500"
+              >
+                <div
+                  class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100"
+                >
                   楽天
                 </div>
               </a>
             </div>
             <div class="mt-4 flex space-x-4 justify-center">
-              <a href="https://bit.ly/3GZ0VMw" target="_blank" class="text-blue-500">
-                <div class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100">
+              <a
+                href="https://bit.ly/3GZ0VMw"
+                target="_blank"
+                class="text-blue-500"
+              >
+                <div
+                  class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100"
+                >
                   HMV
                 </div>
               </a>
-              <a href="https://bit.ly/40qgOT1" target="_blank" class="text-blue-500">
-                <div class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100">
+              <a
+                href="https://bit.ly/40qgOT1"
+                target="_blank"
+                class="text-blue-500"
+              >
+                <div
+                  class="py-2 w-[160px] border border-blue-500 rounded-lg text-center hover:bg-blue-100"
+                >
                   紀伊國屋
                 </div>
               </a>
@@ -158,9 +200,10 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2023年12月3日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p>
-            【日向坂ちゃんねる】<br/>
+            【日向坂ちゃんねる】<br />
             『【緊急招集】不幸が続くのでお祓い行ってきた【ハッピー開運ツアー】』
           </p>
           <iframe
@@ -169,7 +212,7 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
-            class="mt-4 w-full h-60"
+            class="mt-4 w-full h-52"
           ></iframe>
         </div>
       </div>
@@ -178,9 +221,10 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2024年2月11日</p>
+        <hr />
         <div class="mt-2 mx-4">
           <p>
-            【日向坂ちゃんねる】<br/>
+            【日向坂ちゃんねる】<br />
             『日向坂のリトルトゥースがリアル脱出ゲームに挑戦してみた【オードリーANN】』
           </p>
           <iframe
@@ -189,7 +233,7 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
-            class="mt-4 w-full h-60"
+            class="mt-4 w-full h-52"
           ></iframe>
         </div>
       </div>
@@ -198,10 +242,9 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2024年3月8日</p>
+        <hr />
         <div class="mt-2 mx-4">
-          <p class="text-[14px] text-site-color font-bold">
-            初主演ドラマ
-          </p>
+          <p class="text-[14px] text-site-color font-bold">初主演ドラマ</p>
           <p class="text-[20px] text-site-color font-bold">
             「これから配信はじめます」
           </p>
@@ -210,9 +253,10 @@
             <a
               href="https://www.tv-tokyo.co.jp/haihaji/"
               target="_blank"
-              class="font-medium text-blue-500 underline"
-            >「これから配信はじめます」</a
-            > 放送開始。（テレビ東京）<br />
+              class="text-site-color hover:underline"
+              >「これから配信はじめます」</a
+            >
+            放送開始。（テレビ東京）<br />
             全四話のオムニバスドラマ。かすみ役として、第二話（2024/3/15放送）では主演を務めた。
           </p>
         </div>
@@ -222,23 +266,69 @@
     <FadeInOnScroll>
       <div class="mx-2 mb-10">
         <p class="text-sm text-gray-400">2024年7月4日</p>
+        <hr />
         <div class="mt-2 mx-4">
-          <p class="text-[14px] text-site-color font-bold">
-            連続ドラマ初主演
-          </p>
+          <p class="text-[14px] text-site-color font-bold">連続ドラマ初主演</p>
           <p class="text-[20px] text-site-color font-bold">
             「彩香ちゃんは弘子先輩に恋してる」
           </p>
-          <img src="https://www.mbs.jp/ayahiro/images/head.webp" class="mt-4">
+          <img src="https://www.mbs.jp/ayahiro/images/head.webp" class="mt-4" />
           <p class="mt-2">
             連続ドラマ初主演を務める
             <a
-                href="https://www.mbs.jp/ayahiro/"
-                target="_blank"
-                class="font-medium text-blue-500 underline"
-            >「彩香ちゃんは弘子先輩に恋してる」</a
-            > （MBSテレビ系列）放送開始予定！！<br />
+              href="https://www.mbs.jp/ayahiro/"
+              target="_blank"
+              class="text-site-color hover:underline"
+              >「彩香ちゃんは弘子先輩に恋してる」</a
+            >
+            （MBSテレビ系列）放送開始予定！！<br />
             ラブコメディドラマ。主人公・彩香役として、弘子先輩（演：森カンナさん）を一途に想う、不器用ながらも真っ直ぐで可愛らしい役を演じる。
+          </p>
+          <div class="mt-4">
+            <a href="https://www.mbs.jp/ayahiro/blu-ray.shtml" target="_blank">
+              <div
+                class="mx-4 py-2 bg-red-200 rounded-md text-white font-black text-center"
+              >
+                2025年2月14日<br />Blu-ray & DVD BOX 発売決定！🎉
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2024年7月13日</p>
+        <hr />
+        <div class="mt-2 mx-4">
+          <p>
+            【日向坂ちゃんねる】<br />
+            『【お忍び】かとし×こさかなのハウステンボスデート【Vlog】』
+          </p>
+          <iframe
+            src="https://www.youtube.com/embed/Oufuje3Kr9Y?si=dvOzKtxVrU5kS4ni"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            class="mt-4 w-full h-52"
+          ></iframe>
+        </div>
+      </div>
+    </FadeInOnScroll>
+
+    <FadeInOnScroll>
+      <div class="mx-2 mb-10">
+        <p class="text-sm text-gray-400">2024年8月6日</p>
+        <hr />
+        <div class="mt-2 mx-4">
+          <p>
+            <a
+              href="https://www.hinatazaka46.com/s/official/diary/detail/56834?ima=0000&cd=member"
+              class="text-site-color hover:underline"
+              >日向坂46 公式ブログ</a
+            > にて、12thシングルの活動をもって卒業することを発表。
           </p>
         </div>
       </div>
