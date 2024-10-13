b582f774d4ecccd9f18622cfa57d07b8333aec66
Takahashi Shota <shohh6119@gmial.com>
2024-10-14 01:41:05 +0900

    Impl isFinished

---
+++ src/views/components/graduation/messageForm/FinishFormView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-[16px] py-8 px-4 text-center border-2 border-red-200">
+    目標金額に到達したため、募集は終了いたしました！🎉<br />
+    たくさんのご参加、ありがとうございました！<br />
+    <br />
+    詳細は、公演が近づきましたら X にてポストいたします！<br />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
--- src/views/components/graduation/messageForm/index.ts
+++ src/views/components/graduation/messageForm/index.ts
@@ -2,5 +2,6 @@ import AllMessageForm from './AllMessageForm.vue';
 import SummaryArea from './SummaryArea.vue';
 import FAQDropdown from './FAQDropdown.vue';
 import QuestionItem from "./QuestionItem.vue";
+import FinishFormView from "./FinishFormView.vue";
 
-export { AllMessageForm, SummaryArea, FAQDropdown, QuestionItem };
+export { AllMessageForm, SummaryArea, FAQDropdown, QuestionItem, FinishFormView };
