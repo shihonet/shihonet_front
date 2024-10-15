56cbc6966a6484110c56d9288531d6a666b44d8c
Takahashi Shota <shohh6119@gmial.com>
2024-10-16 00:52:57 +0900

    Remove message_form from top

--- src/views/layouts/TopLayout.vue
+++ src/views/layouts/TopLayout.vue
@@ -16,40 +16,17 @@
     <img src="@/assets/images/top_image.png" />
   </FadeInOnScroll>
 
-  <FadeInOnScroll>
-    <div class="mt-40 mx-6">
-      <ChangeFontToCaveat
-        class="text-site-color text-center text-lg"
-        :text="'Pick Up'"
-      />
-      <div class="flex justify-center mt-5">
-        <img
-          src="@/assets/images/graduation_message_completed.jpg"
-          class="w-full max-w-80 cursor-pointer"
-          @click="toMessageFormPage"
-        />
-      </div>
-    </div>
-  </FadeInOnScroll>
-
   <TopProfiles class="mt-32" />
   <TopSchedules />
   <TopBlogs />
 </template>
 
 <script setup lang="ts">
-import {
-  ChangeFontToCaveat,
-  FadeInOnScroll /**, BaseModal*/,
-} from "@/views/components/common";
+import { FadeInOnScroll /**, BaseModal*/ } from "@/views/components/common";
 import { TopProfiles, TopBlogs, TopSchedules } from "@/views/components/top";
 import { onMounted, ref } from "vue";
 
 const show = ref(true);
-
-const toMessageFormPage = () => {
-  window.location.href = "/graduation/messages/form";
-};
 
 onMounted(() => {
   show.value = false;
